fac18b95145888e6f76d6d58134213f2e4e163f5
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2021-08-02 18:01:43 +0200

    Changed home component

--- Angular-FirstProject/Angular-FirstProject/ClientApp/src/app/app.module.ts
+++ Angular-FirstProject/Angular-FirstProject/ClientApp/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -17,8 +15,6 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },      
     ])
