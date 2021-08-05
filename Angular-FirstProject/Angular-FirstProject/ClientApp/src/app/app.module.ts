4f9b687e42a95de69cf180907c1eb1dff24d2196
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2021-08-05 14:05:14 +0200

    Adding components

--- Angular-FirstProject/Angular-FirstProject/ClientApp/src/app/app.module.ts
+++ Angular-FirstProject/Angular-FirstProject/ClientApp/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
@@ -12,12 +18,18 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
    
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },      
+      { path: '', component: HomeComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
