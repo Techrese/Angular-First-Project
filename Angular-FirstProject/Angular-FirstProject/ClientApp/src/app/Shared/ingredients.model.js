46af73737913be5e83d447ec41d98dd4a6d92d54
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2021-08-05 17:33:55 +0200

    changes to mixed componets app and recipes added shopping-list logic

---
+++ Angular-FirstProject/Angular-FirstProject/ClientApp/src/app/Shared/ingredients.model.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Ingredient = void 0;
+var Ingredient = /** @class */ (function () {
+    function Ingredient(name, amount) {
+        this.name = name;
+        this.amount = amount;
+        this.name = name;
+        this.amount = amount;
+    }
+    return Ingredient;
+}());
+exports.Ingredient = Ingredient;
+//# sourceMappingURL=ingredients.model.js.map
